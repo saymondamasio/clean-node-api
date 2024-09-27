c6c213966be2a3f2b3308247ff95d6b9c2a4af88
Saymon Damásio <saymon.damasio95@gmail.com>
2024-09-27 19:46:47 -0300

    chore: add eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 import { FlatCompat } from '@eslint/eslintrc'
 
-const compat = new FlatCompat();
+const compat = new FlatCompat()
 
 export default [
   ...compat.extends('eslint-config-standard'),
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  {
+    files: ['**/*.{js,ts}'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true
+        }
+      ]
+    }
+  },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+  ...tseslint.configs.recommended
+]
