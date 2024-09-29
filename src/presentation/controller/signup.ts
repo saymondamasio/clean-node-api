9ef9bc7c84ae82e60d6e29bbfc57bd433c33b99a
Saymon Damásio <saymon.damasio95@gmail.com>
2024-09-28 21:19:09 -0300

    feat: ensure SignUpController return 400 if password confirmation fails

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -19,7 +19,11 @@ export class SignUpController implements Controller {
         }
       }
 
-      const { email } = httpRequest.body
+      const { email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
 
       const isValidEmail = this.emailValidator.isValid(email)
 
